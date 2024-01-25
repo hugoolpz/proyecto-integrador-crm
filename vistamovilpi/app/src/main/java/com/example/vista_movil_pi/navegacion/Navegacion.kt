b444b6503edb59cdce109a4b68779885b5179cb5
hugoolpz <hugolopezfernandez13@gmail.com>
2024-01-25 10:55:16 +0100

    Commiteao

--- vistamovilpi/app/src/main/java/com/example/vista_movil_pi/navegacion/Navegacion.kt
+++ vistamovilpi/app/src/main/java/com/example/vista_movil_pi/navegacion/Navegacion.kt
@@ -1,4 +1,5 @@
 package com.example.vista_movil_pi.navegacion
+
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,22 +10,21 @@ import com.example.vista_movil_pi.vista.pantallas.Factura
 import com.example.vista_movil_pi.vista.pantallas.ListadoFacturas
 
 
-
 @Composable
-fun Navegacion(){
+fun Navegacion() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Vistas.FormFactura.ruta) {
+    NavHost(navController = navController, startDestination = Vistas.Factura.ruta) {
         composable("${Vistas.ListadoFacturas.ruta}") {
             ListadoFacturas(
                 navController = navController,
             )
         }
-                composable("${Vistas.Factura.ruta}") {
+        composable("${Vistas.Factura.ruta}") {
             Factura(
                 navController = navController,
             )
         }
-                composable("${Vistas.FormFactura.ruta}") {
+        composable("${Vistas.FormFactura.ruta}") {
             FormFactura(
                 navController = navController,
             )
--- vistamovilpi/app/src/main/java/com/example/vista_movil_pi/navegacion/Vistas.kt
+++ vistamovilpi/app/src/main/java/com/example/vista_movil_pi/navegacion/Vistas.kt
@@ -3,6 +3,6 @@ package com.example.vista_movil_pi.navegacion
 sealed class Vistas(val ruta: String) {
     object ListadoFacturas: Vistas("ListadoFacturas")
     object FormFactura: Vistas("FormFactura")
-        object Factura: Vistas("Factura")
+    object Factura: Vistas("Factura")
 
 }
